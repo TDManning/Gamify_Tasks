b6942d9892440bae2b0e6ce87127e8410c21cfcb
Terra Manning <terradmanning@gmail.com>
2025-03-09 10:36:12 -0600

    feat: request spec set up

--- spec/requests/user_rewards_spec.rb
+++ spec/requests/user_rewards_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "UserRewards", type: :request do
-  describe "GET /user_rewards" do
-    it "works! (now write some real specs)" do
-      get user_rewards_index_path
-      expect(response).to have_http_status(200)
-    end
+RSpec.describe "UserRewards API", type: :request do
+  let!(:user) { create(:user) }
+  let!(:reward) { create(:reward) }
+  let!(:user_reward) { create(:user_reward, user: user, reward: reward) }
+
+  let(:valid_attributes) do
+    {
+      user_id: user.id,
+      reward_id: reward.id,
+      purchased: true,
+      unlocked: false
+    }
+  end
+
+  describe "GET /api/v1/user_rewards" do
+  end
+
+  describe "POST /api/v1/user_rewards" do
+  end
+
+  describe "PATCH /api/v1/user_rewards/:id" do
+  end
+
+  describe "DELETE /api/v1/user_rewards/:id" do
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe "Users API", type: :request do
+  let!(:user) { create(:user) }
+
+  describe "GET /api/v1/users" do
+  end
+
+  describe "POST /api/v1/users" do
+    let(:valid_attributes) do
+      attributes_for(:user)
     end
+  end
+
+  describe "PATCH /api/v1/users/:id" do
+  end
+
+  describe "DELETE /api/v1/users/:id" do
   end
 end
