1d6da4f3f08465592b7c0bd0c492189cade0e12a
Terra Manning <terradmanning@gmail.com>
2025-03-09 18:30:04 -0600

    feat: add application controller file with a current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_request
+
+  def authorize_request
+    header = request.headers['Authorization']
+    if header
+      token = header.split(' ').last
+      decoded = AuthService.decode(token)
+      @current_user = User.find(decoded[:user_id]) if decoded
+    else
+      render json: { error: 'Not authorized' }, status: :unauthorized
+    end
+  rescue JWT::DecodeError
+    render json: { error: 'Invalid token' }, status: :unauthorized
+  end
+
+  def current_user
+    @current_user
+  end
 end
